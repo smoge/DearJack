35befcad14480e1716e8af758332caf45f7673e0
Bernardo Barros <bernardo.barros@protonmail.ch>
2024-06-16 15:46:00 -0400

    fix includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 
 
 #include "imgui.h"
-#include "../external/imgui/backends/imgui_impl_glfw.h"
-
-#include "../external/imgui/backends/imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include "imgui_impl_opengl3.h"
 #include <jack/jack.h>
 #include <GLFW/glfw3.h>
